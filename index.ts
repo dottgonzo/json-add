3212adadcbf9d82f0f86170bae244ccf0d1e0ff8
Dario Caruso <darioyzf@gmail.com>
2015-12-26 23:27:26 +0100

    tss

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as diff from "deep-diff";
 
-module.exports=function(original:Object,addon:Object){
+export=function(original:Object,addon:Object){
   diff.observableDiff(original, addon, function (d) {
     // Apply all changes except those to the 'name' property...
     if (d.kind != 'D') {
